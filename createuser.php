9578fe150ede321fc2ea460202c7bbe5a68ee032
swathi7k <93930628+swathi7k@users.noreply.github.com>
2021-11-12 21:12:39 +0530

    Add files via upload

---
+++ createuser.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create User</title>
+     <!-- Google fonts -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+    rel="stylesheet">
+    <!-- Fontawesome tags -->
+    <script src="https://kit.fontawesome.com/63a64c392b.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/table.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/createuser.css">
+    <style>
+      h6 {
+     color: orange;
+    font-style: italic;
+    }
+      </style>
+</head>
+
+<body style="background-color : lightgreen;">
+<?php
+    include 'config.php';
+    if(isset($_POST['submit'])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $balance=$_POST['balance'];
+    $sql="insert into users(name,email,balance) values('{$name}','{$email}','{$balance}')";
+    $result=mysqli_query($conn,$sql);
+    if($result){
+               echo "<script> alert('Congo!! User created');
+                               window.location='transfermoney.php';
+                     </script>";
+                    
+    }
+  }
+?>
+
+<?php
+  include 'navbar.php';
+?>
+
+        <h2 class="text-center pt-4" style="color : red;">Create a User</h2>
+        <br>
+
+  <div class="background">
+  <div class="container">
+    <div class="screen">
+      <div class="screen-header">
+        <div class="screen-header-right">
+          <div class="screen-header-ellipsis"></div>
+          <div class="screen-header-ellipsis"></div>
+          <div class="screen-header-ellipsis"></div>
+        </div>
+      </div>
+      <div class="screen-body">
+        <div class="screen-body-item left">
+          <img class="img-fluid" src="img/user3.jpg" style="border: none; border-radius: 50%;">
+        </div>
+        <div class="screen-body-item">
+          <form class="app-form" method="post">
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="NAME" type="text" name="name" required>
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="EMAIL" type="email" name="email" required>
+            </div>
+            <div class="app-form-group">
+              <input class="app-form-control" placeholder="BALANCE" type="number" name="balance" required>
+            </div>
+            <br>
+            <div class="app-form-group button">
+              <input class="app-form-button" type="submit" value="CREATE" name="submit"></input>
+              <input class="app-form-button" type="reset" value="RESET" name="reset"></input>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<footer id=footerr class="text-center" style="background-color : #EAF0F1; padding: 40px 15%;">
+    <i class="social-media fab fa-twitter"></i>
+    <i class="social-media fab fa-facebook"></i>
+    <i class="social-media fab fa-instagram"></i>
+    <i class="social-media fas fa-envelope"></i>
+     <br>
+     <h6>© Copyright 2021 INDIAN BANK
+    <br>
+    for further queries
+    <br> 
+    contact : 9876543210
+    <br>
+    By Swathi
+    <br>
+    Web development Intern, The Spark Foundation
+    </h6>
+</footer>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
